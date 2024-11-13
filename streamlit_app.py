a62fe4fa5be317d6e6dc3c1cd7aa332162d3c456
ParthM01 <paarthmishra1@gmail.com>
2024-11-13 21:04:20 +0530

    Update streamlit_app.py
    added the pd_df in the code

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,10 +17,10 @@ st.write("The name on your smoothie Will be", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 # session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('search_on'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
-
+pd_df =my_dataframe.to_pandas()
 
 ingredients_select = st.multiselect(
     "Choose upto 5 ingredient",
